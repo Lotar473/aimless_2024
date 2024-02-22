aa70f30b02818c021bd9a0e8983809ddb772c57b
LotaR <gimyuye580@gmail.com>
2024-02-22 16:38:42 +0900

    aimlessCorpseChest.java Modified

--- src/main/java/org/enteras/project_lostar/aimlessCorpseChest.java
+++ src/main/java/org/enteras/project_lostar/aimlessCorpseChest.java
@@ -1,14 +1,11 @@
 package org.enteras.project_lostar;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -32,13 +29,21 @@ public class aimlessCorpseChest implements Listener {
 
         Inventory chestInventory = chest.getBlockInventory();
 
-        // 플레이어의 갑옷을 갑옷 거치대에 추가
-        addArmorStand(player, chest, deathLocation);
-
-        // 플레이어의 인벤토리 아이템을 상자에 추가
+        // 플레이어의 아이템을 상자에 추가
         for (ItemStack item : player.getInventory().getContents()) {
             if (item != null && item.getType() != Material.AIR) {
-                chestInventory.addItem(item);
+                if (chestInventory.firstEmpty() != -1) {
+                    chestInventory.addItem(item);
+                } else {
+                    player.getWorld().dropItemNaturally(deathLocation, item);
+                }
+            }
+        }
+
+        // 핫바 슬롯의 아이템을 드롭
+        for (ItemStack item : player.getInventory().getExtraContents()) {
+            if (item != null && item.getType() != Material.AIR) {
+                player.getWorld().dropItemNaturally(deathLocation, item);
             }
         }
 
@@ -54,26 +59,5 @@ public class aimlessCorpseChest implements Listener {
             return (Chest) blockState;
         }
         return null;
-    }
-
-    private void addArmorStand(Player player, Chest chest, Location location) {
-        ArmorStand armorStand = (ArmorStand) location.getWorld().spawnEntity(location.add(0.5, 1, 0.5), EntityType.ARMOR_STAND);
-        armorStand.setVisible(true);
-        armorStand.setSmall(false);
-        armorStand.setBasePlate(false);
-        armorStand.setArms(true);
-        armorStand.setInvulnerable(false);
-
-        // 갑옷을 갑옷 거치대에 장착
-        armorStand.setHelmet(player.getInventory().getHelmet());
-        armorStand.setChestplate(player.getInventory().getChestplate());
-        armorStand.setLeggings(player.getInventory().getLeggings());
-        armorStand.setBoots(player.getInventory().getBoots());
-
-        // 플레이어의 갑옷 제거
-        player.getInventory().setHelmet(null);
-        player.getInventory().setChestplate(null);
-        player.getInventory().setLeggings(null);
-        player.getInventory().setBoots(null);
     }
 }
